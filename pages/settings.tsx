f17228c30da70e54f24df428382738c79aa250ba
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2021-12-23 19:55:47 +0100

    Route with auth guarding

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,26 +1,30 @@
 import Content from '../components/utils/Content';
+import { useSession, getSession } from "next-auth/react"
+import AuthGuard from '../components/auth-guard';
 
 // noinspection JSUnusedGlobalSymbols
 export default function Settings() {
   return (
-    <Content>
-      <div className="relative p-5 space-y-3">
-        <h1 className="settings-title">Settings</h1>
+    <AuthGuard>
+      <Content>
+        <div className="relative p-5 space-y-3">
+          <h1 className="settings-title">Settings</h1>
 
-        <ul>
-          <li>
-            <span className="settings-component-title">Select theme:</span>
-            <select
-              v-model="$colorMode.preference"
-              className="text-md font-bold rounded border-2 border-purple-300 text-gray-600 p-2 bg-gray-50 hover:border-gray-400 focus:outline-none appearance-none"
-            >
-              <option value="system">System</option>
-              <option value="light">Light</option>
-              <option value="dark">Dark</option>
-            </select>
-          </li>
-        </ul>
-      </div>
-    </Content>
+          <ul>
+            <li>
+              <span className="settings-component-title">Select theme:</span>
+              <select
+                v-model="$colorMode.preference"
+                className="text-md font-bold rounded border-2 border-purple-300 text-gray-600 p-2 bg-gray-50 hover:border-gray-400 focus:outline-none appearance-none"
+              >
+                <option value="system">System</option>
+                <option value="light">Light</option>
+                <option value="dark">Dark</option>
+              </select>
+            </li>
+          </ul>
+        </div>
+      </Content>
+    </AuthGuard>
   );
 }
